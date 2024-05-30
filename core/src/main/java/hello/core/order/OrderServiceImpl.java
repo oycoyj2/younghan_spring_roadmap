8e2e78e1466f032e4cd7fbf9f03236683f3b7c80
oycoyj2 <oycoyj2@gmail.com>
2024-05-30 10:05:45 +0900

    주문 서비스 인터페이스 및 구현 객체 추가

---
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,22 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    @Override
+
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member, itemPrice); //member를 통으로 넘길까 등급만 넘길까는 고민해볼 부분
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+}
